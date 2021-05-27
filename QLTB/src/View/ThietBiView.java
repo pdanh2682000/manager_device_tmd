cefbcb2622e3b8f31e92296f02a39486907a945a
pdanh2682000 <82626385+pdanh2682000@users.noreply.github.com>
2021-05-27 21:35:29 +0700

    commit to try
    my home

--- QLTB/src/View/ThietBiView.java
+++ QLTB/src/View/ThietBiView.java
@@ -21,6 +21,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
@@ -33,6 +34,9 @@ public class ThietBiView extends JFrame {
 	private JTextField textTB;
 	private JTextField textBT;
 	private JTextField textNhap;
+	private JTextField textModel;
+	private JTextField textCompany;
+	private JTextField textCountry;
 
 	/**
 	 * Launch the application.
@@ -54,6 +58,9 @@ public class ThietBiView extends JFrame {
 	 * Create the frame.
 	 */
 	public ThietBiView() {
+		JComboBox comboMaP = new JComboBox();
+		JComboBox comboMaTT = new JComboBox();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 400);
 		setLocationRelativeTo(null);
@@ -77,12 +84,12 @@ public class ThietBiView extends JFrame {
 		
 		JLabel labelNhap = new JLabel("Ngày nhập");
 		labelNhap.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		labelNhap.setBounds(312, 66, 87, 20);
+		labelNhap.setBounds(278, 70, 87, 20);
 		contentPane.add(labelNhap);
 		
 		JLabel labelMaP = new JLabel("Mã Phòng");
 		labelMaP.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		labelMaP.setBounds(312, 139, 71, 22);
+		labelMaP.setBounds(312, 206, 71, 22);
 		contentPane.add(labelMaP);
 		
 		JLabel labelBT = new JLabel("Hạn BT");
@@ -96,7 +103,7 @@ public class ThietBiView extends JFrame {
 		textTB.setColumns(10);
 		
 		textBT = new JTextField();
-		textBT.setBounds(405, 66, 132, 24);
+		textBT.setBounds(352, 65, 64, 24);
 		contentPane.add(textBT);
 		textBT.setColumns(10);
 		
@@ -126,24 +133,42 @@ public class ThietBiView extends JFrame {
 					}
 				}
 				// database
-				insertDB();
+				String maTB = new String("");
+				int indexMaTB = 0;
+				ArrayList<String> arr = getAllMaTB(result);
+				if(!arr.isEmpty()) {
+					String tail = arr.get(arr.size()-1).substring(result.length());
+					if(tail.equals("")) indexMaTB = 1;
+					else indexMaTB = Integer.valueOf(tail) + 1;
+//					System.out.println(tail);
+					maTB = result.concat(String.valueOf(indexMaTB));
+				}
+				else maTB = result + "1";
+				
+				String tenTB = textTB.getText();
+				String maPhong = getMaPhong(comboMaP.getSelectedIndex());
+				String maTT = getMaTT(comboMaTT.getSelectedIndex());
+				String ngayNhap = textNhap.getText();
+				String hanBT = textBT.getText();
+				String model = textModel.getText();
+				String company = textCompany.getText();
+				String country = textCountry.getText();
+				insertDB_TB(maTB, tenTB, maPhong, maTT, ngayNhap, hanBT, model, company, country);
 			}
 		});
 		btnAdd.setBounds(32, 288, 113, 30);
 		contentPane.add(btnAdd);
 		
-		JComboBox comboMaP = new JComboBox();
-		comboMaP.setModel(new DefaultComboBoxModel(new String[] {"P01 (Phòng 1)", "P02 (Phòng 2)", "P03 (Phòng 3)"}));
-		comboMaP.setBounds(405, 139, 132, 23);
+		comboMaP.setModel(new DefaultComboBoxModel(new String[] {"A1 (Phòng A1)", "A2 (Phòng A2)", "A3 (Phòng A3)"}));
+		comboMaP.setBounds(393, 204, 132, 23);
 		contentPane.add(comboMaP);
 		
-		JComboBox comboMaTT = new JComboBox();
 		comboMaTT.setModel(new DefaultComboBoxModel(new String[] {"TT00 (Tốt)", "TT01 (Lỗi)", "TT02 (Hư)"}));
 		comboMaTT.setBounds(104, 207, 145, 22);
 		contentPane.add(comboMaTT);
 		
 		textNhap = new JTextField();
-		textNhap.setBounds(104, 136, 145, 25);
+		textNhap.setBounds(104, 136, 57, 25);
 		contentPane.add(textNhap);
 		textNhap.setColumns(10);
 		
@@ -152,6 +177,36 @@ public class ThietBiView extends JFrame {
 		lblNewLabel.setForeground(new Color(0, 0, 0));
 		lblNewLabel.setBounds(229, 11, 226, 22);
 		contentPane.add(lblNewLabel);
+		
+		JLabel labelModel = new JLabel("Model");
+		labelModel.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		labelModel.setBounds(209, 143, 46, 14);
+		contentPane.add(labelModel);
+		
+		textModel = new JTextField();
+		textModel.setBounds(265, 139, 86, 25);
+		contentPane.add(textModel);
+		textModel.setColumns(10);
+		
+		JLabel labelCompany = new JLabel("Company");
+		labelCompany.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		labelCompany.setBounds(374, 141, 57, 14);
+		contentPane.add(labelCompany);
+		
+		textCompany = new JTextField();
+		textCompany.setBounds(460, 138, 86, 26);
+		contentPane.add(textCompany);
+		textCompany.setColumns(10);
+		
+		JLabel labelCountry = new JLabel("Country");
+		labelCountry.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		labelCountry.setBounds(438, 70, 57, 14);
+		contentPane.add(labelCountry);
+		
+		textCountry = new JTextField();
+		textCountry.setBounds(498, 64, 71, 26);
+		contentPane.add(textCountry);
+		textCountry.setColumns(10);
 	}
 	
 	public Connection getConnection(String host, String port, String dbName, String username,
@@ -169,18 +224,46 @@ public class ThietBiView extends JFrame {
 		return conn;
 	}
 	
-	public void insertDB() {
+	public ArrayList<String> getAllMaTB(String header){
+		ArrayList<String> arr = new ArrayList<String>();
 		Connection conn = getConnection("localhost", "5432", "QLTB", "postgres", "123");
 		if(conn != null) {
 			Statement st = null;
 			ResultSet rs = null;
+			try {
+				st = conn.createStatement();
+				String sqlQuery = "SELECT matb FROM public.thietbi WHERE matb ~ " + "\'^" +header + "\'";
+				rs = st.executeQuery(sqlQuery);
+				while(rs.next()) {
+					arr.add(rs.getString(1));
+				}
+				conn.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return arr;
+	}
+	
+	public void insertDB_TB(String maTB, String tenTB, String maPhong, String maTT, String ngayNhap,
+			String hanBT, String model, String company, String country) {
+		Connection conn = getConnection("localhost", "5432", "QLTB", "postgres", "123");
+		if(conn != null) {
+			Statement st = null;
 			PreparedStatement  ps = null;
 			try {
 				st = conn.createStatement();
-				String sqlQuery = "INSERT INTO public.phong VALUES (?, ?)";
+				String sqlQuery = "INSERT INTO public.thietbi VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 				ps = conn.prepareStatement(sqlQuery);
-				ps.setString(1, "test");
-				ps.setString(2, "test");
+				ps.setString(1, maTB);
+				ps.setString(2, tenTB);
+				ps.setString(3, maPhong);
+				ps.setString(4, maTT);
+				ps.setString(5, ngayNhap);
+				ps.setString(6, hanBT);
+				ps.setString(7, model);
+				ps.setString(8, company);
+				ps.setString(9, country);
 				ps.execute();
 			}
 			catch(Exception e) {
@@ -189,38 +272,21 @@ public class ThietBiView extends JFrame {
 		}
 	}
 	
-//	public void connectDB() {
-//		String host="localhost";
-//		String port="5432";
-//		String dbname="QLTB";
-//		String user="postgres";
-//		String pass="123";
-//		String dburl = "jdbc:postgresql://"+host+":"+port+"/"+dbname+"?loggerLevel=OFF";
-//		Connection conn = null;
-//		Statement stmt = null;
-//		PreparedStatement pstmt = null;
-//		ResultSet rs = null;
-//        try {
-//            conn = DriverManager.getConnection(dburl, user, pass);
-//            System.out.println("connect successfully!");
-//        } catch (Exception ex) {
-//            System.out.println("connect failure!");
-//            ex.printStackTrace();
-//        }
-//        
-//        try {
-//            // create statement
-//            stmt = conn.createStatement();
-//            // statement
-//            rs = stmt.executeQuery("select * from public.phong");
-//            // show data
-//            while (rs.next()) {
-//                System.out.println(rs.getString(1) + "  " + rs.getString(2));
-//            }
-//            // close connection
-//            conn.close();
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
-//	}
+	
+	public String getMaTT(int index) {
+		switch(index) {
+			case 0: return "TT1";
+			case 1: return "TT2";
+			case 2: return "TT3";
+			default: return "TT1";
+		}
+	}
+	public String getMaPhong(int index) {
+		switch(index) {
+			case 0: return "A1";
+			case 1: return "A2";
+			case 2: return "A3";
+			default: return "A1";
+		}
+	}
 }
